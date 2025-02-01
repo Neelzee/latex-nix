823ab4967c4c9f06e04b24a76cdf6ebb8d9fba66
Neelzee <nilsien2001@gmail.com>
2025-02-01 14:01:31 +0100

    fix: can now use citations

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Latex Nix Script";
+  description = "Latex Nix";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
@@ -15,17 +15,18 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        # Using full scheme because I am too lazy to figure out what packages I need
         tex = pkgs.texlive.combined.scheme-full;
+        file = "main";
       in
       rec {
         packages = {
           document = pkgs.stdenvNoCC.mkDerivation rec {
-            name = "latex-document";
+            name = "latex-demo-document";
             src = self;
             buildInputs = [
               pkgs.coreutils
               tex
+              pkgs.biber
             ];
             phases = [
               "unpackPhase"
@@ -36,12 +37,14 @@
               export PATH="${pkgs.lib.makeBinPath buildInputs}";
               mkdir -p .cache/texmf-var
               env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
-                latexmk -interaction=nonstopmode -pdf -lualatex \
-                main.tex
+                pdflatex "${file}.tex"
+              biber ${file}
+              pdflatex "${file}.tex"
+              pdflatex "${file}.tex"
             '';
             installPhase = ''
               mkdir -p $out
-              cp main.pdf $out/
+              cp "${file}".pdf $out/
             '';
           };
         };
